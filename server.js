4e19d6e58cdc225f57a15c1b08d1a9e4a8d66cbf
System <none>
2017-06-05 13:22:26 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ var express = require('express');
 var app = express();
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
 app.use(express.static('views'));
-app.use(express.static('node_modules/three/build'));
-app.use(express.static('node_modules/three/examples/js/renderers'));
+app.use(express.static('node_modules/p5/lib'));
+app.use(express.static('node_modules/p5/lib/addons'));
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
