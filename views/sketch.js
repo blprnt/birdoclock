208aef4828b2c7c53125c6bfd9eab0bb60bcc93d
Glitch (p5js-template) <none>
2019-02-06 16:06:48 +0000

    🏬🏆 Checkpoint ./views/sketch.js:218871/244

--- views/sketch.js
+++ views/sketch.js
@@ -2,17 +2,6 @@
 
 function setup() {
   createCanvas(500, 500);
-  loadJSON("https://www.loc.gov/photos/?q=frog&fo=json", dataLoaded);
-}
-
-function dataLoaded(data) {
-  
-  var urls = data.results[3].image_url;
-  var url = "https:" + urls[urls.length - 1];
-  
-  var img = createImg(url);
-  
-  console.log(url);
 }
 
 function draw() {
