47669a5826a3fbe604612a75e2a77d912ac25411
Glitch (p5js-template) <none>
2019-02-06 18:22:16 +0000

    ✋🙅 Checkpoint ./views/sketch.js:218871/121

--- views/sketch.js
+++ views/sketch.js
@@ -1,9 +1,14 @@
 
-
 function setup() {
   createCanvas(500, 500);
+  loadData("https://www.loc.gov/photos/?q=boy&fo=json");
 }
 
 function draw() {
+  
+}
+
+function loadData(url) {
+  loadJSON(url);
   
 }
